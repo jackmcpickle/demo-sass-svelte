3c4e640e4113ff5bc2bd3666f579a1a27792a3be
Jack McNicol <jack.mcnicol@nqry.com.au>
2024-11-28 08:16:06 +1030

    initial svelte 5 with sass and  include-media

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<style lang="scss">
+    @use '../styles/vars';
+    @use 'include-media/dist/include-media' with (
+        $breakpoints: vars.$breakpoints
+    );
+
+    h1 {
+        @include include-media.media('>sm') {
+            color: red;
+        }
+
+        @include include-media.media('>md') {
+            color: blue;
+        }
+    }
+
+</style>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!--  -->
